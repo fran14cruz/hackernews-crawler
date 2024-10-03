669ac76d0040cdbbf5c9e93912e3ee9addc34c7d
fran14cruz <fran14.ich@gmail.com>
2024-10-03 00:24:02 -0500

    Add filter routes

--- app.py
+++ app.py
@@ -13,6 +13,18 @@ def scrape():
     entries = scrape_hacker_news()
     return jsonify(entries)
 
+@app.route('/filter/more_than_five_words') # route to filter entries
+def filter_more_than_five_words():
+    entries = scrape_hacker_news()
+    filtered = filter_by_word_count(entries, True)
+    return jsonify(filtered)
+
+@app.route('/filter/five_or_less_words')
+def filter_five_or_less_words():
+    entries = scrape_hacker_news()
+    filtered = filter_by_word_count(entries, False)
+    return jsonify(filtered)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
