09d643311a288335c6ad087a5f7b0d85b8cac4f5
fran14cruz <fran14.ich@gmail.com>
2024-10-03 00:09:05 -0500

    create scrape function

---
+++ scraper.py
@@ -0,0 +1,44 @@
+"""
+File name: scraper.py
+Author: Francisco Cruz
+Created: 02-10-2024
+Version: 1.0
+Description: This script contains logic for scraping Hacker News
+"""
+# Import statements
+import requests
+from bs4 import BeautifulSoup
+import re
+
+
+def scrape_hacker_news():
+    """
+    :return: retrieved entries
+    """
+    url = 'https://news.ycombinator.com/'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')  # returns a soup object
+
+    entries = []
+    titles = soup.select('.titleline')
+    subtexts = soup.select('.subtext')
+
+    for i in range(min(30, len(titles))):  # limit to first 30 entries
+        title = titles[i].find('a').get_text()
+
+        subtext = subtexts[i].get_text()
+        points = re.findall(r'(\d+) point', subtext)
+        comments = re.findall(r'(\d+)\s*comment', subtext)  # special character between number and comment
+
+        # parse points and comments to numbers
+        points = int(points[0]) if points else 0
+        comments = int(comments[0]) if comments else 0
+
+        entries.append({
+            'number': i + 1,
+            'title': title,
+            'points': points,
+            'comments': comments
+        })
+
+    return entries
